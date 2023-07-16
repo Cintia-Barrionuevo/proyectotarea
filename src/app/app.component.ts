6929ae473f7e00f7a4775ade29cebd2b2bd737f6
Cintia-Barrionuevo <cinti.barrio@gmail.com>
2023-07-16 09:54:07 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+
+ export class AppComponent {
   title = 'proyectotarea';
+
+
+  tablaProductos = [
+    {nombre:'Lapicera',costo:'150'},
+    {nombre:'Cartuchera',costo:'1500'},
+    {nombre:'Temperas',costo:'500'},
+    {nombre:'Pincel',costo:'100'},
+  ];
+
+  mostrarLista: boolean = true;
+  ocultarLista() {
+    this.mostrarLista = !this.mostrarLista;
+  }
+  
+
+
 }
